8fd44b7ad7462a4ca82671d851a7deb46cba1f72
Phantom1000 <37048875+Phantom1000@users.noreply.github.com>
2020-12-06 02:07:53 +0300

    test

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -33,6 +33,8 @@ class MainController extends Controller
             }
         }
 
+        return 'test';
+
         $products = $query->paginate(6);
         return view('index', compact('products', 'params'));
     }
